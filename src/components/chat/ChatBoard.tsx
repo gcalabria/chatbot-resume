7cc9f9d345a83c46d6449687527790cbd604e6f4
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2024-04-09 23:48:58 +0200

    Scroll to the bottom when new message arrives

--- src/components/chat/ChatBoard.tsx
+++ src/components/chat/ChatBoard.tsx
@@ -1,13 +1,49 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
 import ChatMessage from "./ChatMessage";
 import { Send } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { fakeMessages } from "@/mocks";
 import { Message } from "@/interfaces";
+import ChatLoadingMessage from "./ChatLoadingMessage";
+import React from "react";
 
 function ChatBoard() {
   const [messages] = useState<Message[]>(fakeMessages);
+  const [inputMessage, setInputMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const messagesEndRef = useRef<null | HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    setTimeout(
+      () =>
+        messagesEndRef.current?.scrollIntoView({
+          behavior: "smooth",
+        }),
+      0
+    );
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setInputMessage(event.currentTarget.value);
+  };
+
+  const handleSendMessage = (event: React.FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+    messages.push({
+      id: `${messages.length + 1}`,
+      author: "HUMAN",
+      text: inputMessage,
+    });
+    setInputMessage("");
+    scrollToBottom();
+  };
 
   let content;
   if (messages.length === 0) {
@@ -20,17 +56,24 @@ function ChatBoard() {
     <div className="flex flex-col w-full h-screen">
       <div className="flex flex-col flex-grow gap-4 p-2 overflow-auto ">
         {content}
+        {isLoading && <ChatLoadingMessage />}
+        <div ref={messagesEndRef} />
       </div>
-      <div className="flex flex-row items-center p-2 border-t-2">
+      <form
+        className="flex flex-row items-center px-2 py-4 border-t-2"
+        onSubmit={handleSendMessage}
+      >
         <Input
           placeholder="Ask me something..."
           id="chatInput"
           className="mr-2"
+          onChange={handleInputChange}
+          value={inputMessage}
         />
-        <Button variant="outline" size="icon">
+        <Button type="submit" variant="outline" size="icon">
           <Send className="h-4 w-4" />
         </Button>
-      </div>
+      </form>
     </div>
   );
 }
