895b21a0d20fdad00419d26def1e73632fbcf4f9
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2024-04-09 23:22:30 +0200

    Add ChatLoadingMessage

---
+++ src/components/chat/ChatLoadingMessage.tsx
@@ -0,0 +1,26 @@
+import { Alert, AlertDescription } from "@/components/ui/alert";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Loader2 } from "lucide-react";
+
+const Icons = {
+  spinner: Loader2,
+};
+
+function ChatLoadingMessage() {
+  return (
+    <div className={"flex self-start mr-20"}>
+      <Avatar className="mr-4 h-8 w-8">
+        <AvatarImage src="https://github.com/shadcn.png" />
+        {/* <AvatarFallback>{message.author}</AvatarFallback> */}
+      </Avatar>
+
+      <Alert className={"flex-wrap p-2"}>
+        <AlertDescription>
+          <Icons.spinner className="h-4 w-4 animate-spin" />
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
+}
+
+export default ChatLoadingMessage;
