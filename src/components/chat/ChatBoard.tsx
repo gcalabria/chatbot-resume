31b0202313f28661ae85d73d33dc45511db88fbc
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2024-04-10 20:29:47 +0200

    Fix spelling

--- src/components/chat/ChatBoard.tsx
+++ src/components/chat/ChatBoard.tsx
@@ -6,7 +6,17 @@ import { Button } from "@/components/ui/button";
 import { Message } from "@/interfaces";
 import ChatLoadingMessage from "./ChatLoadingMessage";
 import React from "react";
-import { chain } from "@/langchain/langchain";
+import {
+  chain,
+  // embeddings,
+  // splitter,
+  // supabaseClient,
+} from "@/langchain/langchain";
+// import axios from "axios";
+// import { linkedInProfileToText } from "@/langchain/utils";
+// import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+// import { SUPABASE_TABLE_NAME } from "@/constants";
+// import { fakeProfile } from "@/mocks";
 
 function ChatBoard() {
   const [messages] = useState<Message[]>([]);
@@ -57,9 +67,23 @@ function ChatBoard() {
     setIsLoading(false);
     scrollToBottom();
 
+    // // Crawl linkedin profile
+    // const response = await axios.get("https://linkedin-api8.p.rapidapi.com/", {
+    //   headers: {
+    //     "X-RapidAPI-Key": "b551f4841amsh8279ccf4c2a4582p1937c2jsnd5fea3ce5a6b",
+    //     "X-RapidAPI-Host": "linkedin-api8.p.rapidapi.com",
+    //   },
+    //   params: {
+    //     username: "gcalabria",
+    //   },
+    // });
+
+    // const profile = response.data;
+
     // // Add documents (linkedin profile) to supabase vectorstore
     // const profile = fakeProfile;
     // const text = linkedInProfileToText(profile);
+    // console.log(text);
     // const output = await splitter.createDocuments([text]);
     // console.log(output);
 
@@ -80,7 +104,7 @@ function ChatBoard() {
     content = (
       <div className="flex flex-col flex-grow items-center justify-center">
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-          Ask me something about me career :)
+          Ask me anything about my career :)
         </h2>
       </div>
     );
