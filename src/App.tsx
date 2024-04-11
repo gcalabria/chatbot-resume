f1ca41a0bca718dcdbcd63799226340c84cc8b02
Guilherme Calabria Lopes <guicalabriadev@gmail.com>
2024-04-11 12:06:04 +0200

    Clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-// import { Button } from "@/components/ui/button";
-// import { useState } from "react";
-// import { fakeResponse } from "./mocks";
-// import { LinkedInProfile } from "./interfaces";
 import { ThemeProvider } from "./components/ThemeProvider";
 import ChatBoard from "./components/chat/ChatBoard";
-// import axios from "axios";
 
 function App() {
-  // const [profile, setProfile] = useState<LinkedInProfile | null>(null);
-  // const handleClick = async () => {
-  //   // const response = await axios.get("https://linkedin-api8.p.rapidapi.com/", {
-  //   //   headers: {
-  //   //     "X-RapidAPI-Key": "b551f4841amsh8279ccf4c2a4582p1937c2jsnd5fea3ce5a6b",
-  //   //     "X-RapidAPI-Host": "linkedin-api8.p.rapidapi.com",
-  //   //   },
-  //   //   params: {
-  //   //     username: "gcalabria",
-  //   //   },
-  //   // });
-  //   const response = fakeResponse;
-  //   const _profile = response.data;
-  //   setProfile(_profile);
-  // };
-
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <ChatBoard />
